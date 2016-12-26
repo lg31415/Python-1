3cb6930d69803a70d509db1e3d4c402829746e6a
tuling56 <tuling56@gmail.com>
2016-12-26 17:46:56 +0800

    完善并测试通过弹幕服务器日志分析的mysql实现程序（文件保存为json格式，处理了中文数据）

--- Projects/barrage_stat/barrage_stat.py
+++ Projects/barrage_stat/barrage_stat.py
@@ -11,6 +11,7 @@ import os, sys
 from datetime import date, datetime, timedelta
 import pandas as pd
 import numpy as np
+import json
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -142,51 +143,53 @@ def data_stat_pandasql(flog_proc):
 '''
 import MySQLdb
 def data_stat_mysql(flog_proc):
-	conn=MySQLdb.connect(host='127.0.0.1',user='root',passwd='root',db='test')
-	cur=conn.cursor()
-	# 创建表和加载数据
-	csql="use test;create table barrage_stat(category varchar(10), groupID varchar(30), type varchar(10), key varchar(100), subkey varchar(30), peerid varchar(20), userid varchar(30), action varchar(10), title varhcar(50))"
-	cur.execute(csql)
-	lsql="use test;load data local infile '%s' into table barrage_stat;" %(flog_proc)
-	cur.execute(lsql)
-	conn.commit()
+	detail_stat=os.path.join(file_path,"data","barrage_stat_detail_"+calcday)
+	f=open(detail_stat,'w')
+	try:
+		conn=MySQLdb.connect(host='127.0.0.1',user='root',passwd='sd-9898w',db='test')
+		cur=conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+		# 创建表和加载数据
+		csql="use test;create table if not exists barrage_stat(category varchar(10), groupID varchar(30), type varchar(10), \`key\` varchar(100), subkey varchar(30), peerid varchar(20), userid varchar(30), action varchar(10), title varchar(50))ENGINE=MyISAM DEFAULT CHARSET=utf8;;"
+		cur.execute(csql)
+		lsql="use test;delete from barrage_stat;load data local infile '%s' into table barrage_stat fields terminated by ',';" %(flog_proc)
+		cur.execute(lsql)
+		conn.commit()
+	except Exception,e:
+		t,value,traceback = sys.exc_info()
+		print str(e)
 
 	#　利用mysql进行数据统计
 	## 获取弹幕ID
 	print "GetID:"
-	qsql="select \"%s\" ,\"getID\",type,count(*) ,count(DISTINCT groupID)+count(DISTINCT conncat(key,subkey)) from test.barrage_stat where category=\"getID\";" % (calcday)
+	qsql="select \"%s\" ,\"getID\",type,count(*) as '次数',count(DISTINCT groupID)+count(DISTINCT concat(`key`,subkey)) as '影片数' from test.barrage_stat where category=\"getID\";" % (calcday)
 	print qsql
 	cur.execute(qsql)
 	data=cur.fetchall()
-	for d in data:
-		print ','.join(list(map(lambda x: str(x), d)))
+	json.dump(data,f)
 
 	## 统计下载弹幕次数和影片数
 	print "下载:"
-	qsql="select \"%s\" ,\"download\",type,count(*) ,count(DISTINCT groupID)+count(DISTINCT conncat(key,subkey))  from test.barrage_stat where category=\"info\";" % (calcday)
+	qsql="select \"%s\" ,\"download\",type,count(*) as '次数',count(DISTINCT groupID)+count(DISTINCT concat(`key`,subkey)) as '影片数'  from test.barrage_stat where category=\"info\";" % (calcday)
 	print qsql
 	cur.execute(qsql)
 	data=cur.fetchall()
-	for d in data:
-		print ','.join(list(map(lambda x: str(x), d)))
+	json.dump(data,f)
 
 	## 统计上报弹幕的次数，人数，影片数
 	print "上报:"
-	qsql="select \"%s\",\"upload\",type,count(*),count(distinct peerid),count(DISTINCT groupID)+count(DISTINCT conncat(key,subkey)) from test.barrage_stat where category=\"upload\" GROUP by category,type;" % (calcday)
+	qsql="select \"%s\",\"upload\",type,count(*) as '次数',count(distinct peerid) as '人数',count(DISTINCT groupID)+count(DISTINCT concat(`key`,subkey)) as '影片数' from test.barrage_stat where category=\"upload\" GROUP by category,type;" % (calcday)
 	print qsql
 	cur.execute(qsql)
 	data=cur.fetchall()
-	for d in data:
-		print ','.join(list(map(lambda x: str(x), d)))
+	json.dump(data,f)
 
 	## 统计点评弹幕的次数，影片数,（细分点赞和举报）
 	print "点评:"
-	qsql="select \"%s\",\"remark\",action,count(*),count(DISTINCT groupID)+count(DISTINCT conncat(key,subkey)) from test.barrage_stat where category=\"remark\" GROUP by category,action;" % (calcday)
+	qsql="select \"%s\",\"remark\",action,count(*) as '次数',count(DISTINCT groupID)+count(DISTINCT concat(`key`,subkey)) as '影片数' from test.barrage_stat where category=\"remark\" GROUP by category,action;" % (calcday)
 	print qsql
 	cur.execute(qsql)
 	data=cur.fetchall()
-	for d in data:
-		print ','.join(list(map(lambda x: str(x), d)))
+	json.dump(data,f)
 
 	## 统计搜索的次数,搜索的影片数
 	print "搜索:"
@@ -194,10 +197,12 @@ def data_stat_mysql(flog_proc):
 	print qsql
 	cur.execute(qsql)
 	data=cur.fetchall()
-	for d in data:
-		print ','.join(list(map(lambda x: str(x), d)))
+	json.dump(data,f)
+	#写入中文的时候用下面的方法
+	#f.write(json.dumps(data,ensure_ascii=False)+"\n")
 
 	conn.close()
+	f.close()
 
 
 '''
