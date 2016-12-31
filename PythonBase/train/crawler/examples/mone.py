a0a086aab808943ce98b4c399cb3e3ac5021a641
tuling56 <tuling56@gmail.com>
2016-12-31 16:48:56 +0800

    爬虫模块增加几个例子

---
+++ PythonBase/train/crawler/examples/mone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+	Fun:
+	Ref:
+	State：
+	Date:2016/12/30
+	Author:tuling56
+'''
+import re, os, sys
+import hues
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def fun():
+	print "fun"
+
+
+if __name__ == "__main__":
+	fun()
+
