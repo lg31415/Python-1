a79b18e786440349e7b9fd922accd517679ad931
tuling56 <tuling56@gmail.com>
2017-04-26 18:01:42 +0800

    flask的restful实现

---
+++ PythonBase/src/web/flask/flask_uwsgi/app_restful/__init__.py
@@ -0,0 +1,2 @@
+from restful import *
+
---
+++ PythonBase/src/web/flask/flask_uwsgi/app_restful/restful.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+	Fun: flask的restful接口实现
+	Ref: head-first-flask
+	State：
+	Date:2017/4/26
+	Author:tuling56
+'''
+import re, os, sys
+import hues
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+    def post(self):
+        return u'post方法尚未定义'
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def put(self):
+        return u'put方法尚未定义'
+
+    # 显示添后的所有
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')        # 整体的修改
+api.add_resource(Todo, '/todos/<todo_id>')  # 单个修改
+
+
+# 测试入口
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
