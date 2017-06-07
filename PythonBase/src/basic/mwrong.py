ebd1cca20b709aeb850c5adca331e6218ba7655b
tuling56 <tuling56@gmail.com>
2017-06-07 20:18:06 +0800

    增加python易错记录mwrong.py文件

--- PythonBase/src/basic/mwrong.py
+++ PythonBase/src/basic/mwrong.py
@@ -2,12 +2,13 @@
 # -*- coding: utf8 -*-
 __author__ = 'yjm'
 '''
-  功能注释：python细节
+  功能注释：python容易出错的地方
 '''
 
 x=1
 def run():
-    #x+=2
+    global x  # 如果不加入个申明，则x=x+2提示错误
+    x=x+2
     y=x+1
     print y
 
