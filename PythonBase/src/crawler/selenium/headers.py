fedf510a5a6d1d1cb74efd854905b8d947db15f9
tuling56 <tuling56@gmail.com>
2017-06-22 20:22:08 +0800

    增加爬虫头，模板化信息

---
+++ PythonBase/src/crawler/selenium/headers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+	Fun:http头定义
+	Ref:
+	State：
+	Date:2017/6/22
+	Author:tuling56
+'''
+
+
+# 设置请求头
+headers=[]
+header_firfox={'User-Agent' : "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko"}
+header_chrome={'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
+header_edge={'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"}
+headers.append(header_firfox)
+headers.append(header_chrome)
+headers.append(header_edge)
+
+# 设置代理
+proxy_ips=[
+	"http://10.10.1.10:3128"
+]
--- PythonBase/src/crawler/selenium/mphantomjs.py
+++ PythonBase/src/crawler/selenium/mphantomjs.py
@@ -3,23 +3,29 @@
 '''
 	Fun:phantomjs无界面浏览器+selenium
 	Ref:http://blog.csdn.net/qq_30242609/article/details/70859891
+		http://blog.csdn.net/tcorpion/article/details/70213435
 	State：
 	Date:2017/4/28
 	Author:tuling56
 '''
 import re, os, sys
 import hues
+import time
+import random
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+import headers
 from selenium import  webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities   # 设置兼容性
+from selenium.webdriver.common.proxy import ProxyType
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-import time
+
 
 '''
-	phantomjs无节面浏览器
+	phantomjs无界面浏览器
 '''
 class mPhantonJS():
 	'''
@@ -27,16 +33,43 @@ class mPhantonJS():
 	'''
 	def __init__(self):
 		try:
-			self.driver=webdriver.PhantomJS()
+			if False:
+				self.driver=webdriver.PhantomJS(desired_capabilities=self.__webdrive_conf())
+			else:
+				self.driver=webdriver.PhantomJS()
 		except Exception,e:
 			self.driver.quit()
-			return False
+			self.driver.close()
+
 		self.driver.get('http://www.baidu.com/')
 
 	def __del__(self):
 		self.driver.quit()
 		self.driver.close()
 
+	# 请求配置
+	def __webdrive_conf(self,imgload=False,isproxy=False):
+		desired_capabilities = DesiredCapabilities.PHANTOMJS.copy()
+		# 从USER_AGENTS列表中随机选一个浏览器头，伪装浏览器
+		desired_capabilities["phantomjs.page.settings.userAgent"] = (random.choice(headers.headers))
+
+		#  不载入图片，爬页面速度会快很多
+		if imgload:
+			desired_capabilities["phantomjs.page.settings.loadImages"] = True
+		else:
+			desired_capabilities["phantomjs.page.settings.loadImages"] = False
+
+		# 设置代理
+		if isproxy:
+			# 利用DesiredCapabilities(代理设置)参数值，重新打开一个sessionId，我看意思就相当于浏览器清空缓存后，加上代理重新访问一次url
+			proxy = webdriver.Proxy()
+			proxy.proxy_type = ProxyType.MANUAL
+			proxy.http_proxy = random.choice(headers.proxy_ips)
+			proxy.add_to_capabilities(desired_capabilities)
+
+		return desired_capabilities
+
+	# 演示
 	def demo(self):
 		print self.driver.title #,driver.page_source
 		assert "百度" in self.driver.title
@@ -52,6 +85,7 @@ class mPhantonJS():
 			assert 0,u"找不到搜索的元素"
 		self.driver.close()
 		self.driver.quit()
+
 
 # 测试入口
 if __name__ == "__main__":
