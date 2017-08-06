80bd855b8237d46cbde6dbdac733d3cc9f4e2d87
tuling56 <tuling56@gmail.com>
2017-08-06 19:15:52 +0800

    实现分组按条件统计（等同于casewhen,stat，group的组合）

--- tools/stat/stat.py
+++ tools/stat/stat.py
@@ -9,6 +9,7 @@
 '''
 import re, os, sys
 import hues
+from collections import defaultdict
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -18,6 +19,7 @@ class PStat():
 		self.src_data='./groupby_data'
 		self.posnum={}
 
+	# 分组统计累和
 	def groupby_sum(self):
 		 with open(self.src_data,'r') as f:
 			for line in f:
@@ -55,9 +57,46 @@ class PStat():
 						self.posnum[pos]=[-1,int(pv),int(uv)]
 		 print self.posnum
 
+	def __judge_range(self,value):
+		value=int(value)
+		if value>=0 and value<1024:
+			return '[0,1)'
+		elif value>=1024 and value<2048:
+			return '[1,2)'
+		elif value>2048 and value<3072:
+			return '[2,3)'
+		elif value>=3072 and value<4096:
+			return '[3,4)'
+		elif value>=4096:
+			return '[4,)'
+		else:
+			return 'error'
+
+
+	# 分组统计类合
+	def groupby_count(self):
+		res=defaultdict(0)
+		with open(self.src_data,'r') as f:
+			num=0
+			for line in f:
+				num=num+1
+				ratio=str(round(num*100/7215226.0,2))+"%"
+				hues.info("Process:"+ratio)
+				try:
+					dt,fu2,fu6,fu7=line.split('\t')
+					dur=self.__judge_range(fu6)
+					flag='yes' if 'geforce' in fu7.lower() else "no"
+					key=dur+"\t"+flag
+					res[key]=res[key]+1
+				except Exception,e:
+					print str(e)
+		f=open("ww_mem_geforce_dist","w")
+		for k,v in res.iteritems():
+			f.write(k+"\t"+str(v)+'\n')
+		f.close()
 
 # 测试入口
 if __name__ == "__main__":
     pstat=PStat()
-    pstat.groupby_sum()
+    pstat.groupby_count()
 
