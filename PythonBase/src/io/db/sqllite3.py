18c56473f665459cfe81f0f2899535e6d9071528
tuling56 <tuling56@gmail.com>
2017-08-24 20:02:49 +0800

    sqllite3学习完善

--- PythonBase/src/io/db/sqllite3.py
+++ PythonBase/src/io/db/sqllite3.py
@@ -3,19 +3,31 @@
 __author__ = 'yjm'
 '''
   功能注释：sqllite3嵌入式关系数据库,其数据库就是一个文件
+  参考：http://www.runoob.com/sqlite/sqlite-tutorial.html
 '''
 
 import sqlite3
 
-def msqlite3():
-    conn=sqlite3.connect('sqllite3_test.db')
-    cursor = conn.cursor()
-    #cursor.execute('create table if NOT EXISTS user (id varchar(20) primary key, name varchar(20))')
-    cursor.execute('insert into user (id, name) values (\'2\', \'zhang\')')
-    print cursor.rowcount
-    cursor.close()
-    conn.commit()
-    conn.close()
+class CSqlite3():
+    def __init__(self):
+        self.conn=sqlite3.connect('sqllite3.db')
+        self.cursor = self.conn.cursor()
+    def __del__(self):
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
 
+    def dcl(self):
+        csql="create table if not exists baseinfo(id int auto_increment primary key, name varchar(20),age smallint,insert_time datetime);"
+        self.cursor.execute(csql)
+
+    def dml(self):
+        insql="insert into baseinfo(name, age,insert_time) values ('张小二',12,datetime())"
+        self.cursor.execute(insql)
+        print self.cursor.rowcount
+
+# 测试入口
 if __name__ == "__main__":
-    msqlite3()
+    msql3=CSqlite3()
+    msql3.dcl()
+    msql3.dml()
