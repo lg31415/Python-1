c399e6bdc90797b32ee41ca12e5fe83ad47b9a8d
tuling56 <tuling56@gmail.com>
2017-08-31 20:19:53 +0800

    正则处理的完善，添加去除html标签的正则表达式

--- tools/string/string_proc.py
+++ tools/string/string_proc.py
@@ -17,26 +17,29 @@ sys.setdefaultencoding('utf-8')
 '''
 	单字符判断
 '''
-#判断单个字符是否是中文
+# 判断单个字符是否是中文
 def is_cn_char(i):
-	return 0x4e00<=ord(i)<0x9fa6
+    return 0x4e00 <= ord(i) < 0x9fa6
+
 
 # 判断但字符是否中文或英文
 def is_cn_or_en(i):
-	o = ord(i)
-	return o<128 or 0x4e00<=o<0x9fa6
+    o = ord(i)
+    return o < 128 or 0x4e00 <= o < 0x9fa6
+
 
 # 判断字符是否是16进制数
 def isxdigit(s):
-	return re.match(r'^[A-Fa-f0-9]{1,}$',s) is not None
+    return re.match(r'^[A-Fa-f0-9]{1,}$', s) is not None
+
 
 # 判断字符是否是日文
 def isJapan(s):
-	'''
+    '''
 	unicode
 	'''
-	#return re.search(ur"[\u3040-\u309f]+",unicode(s,"utf-8",'ignore')) is not None or re.search(ur"[\u30a0-\u30ff]+",unicode(s,"utf-8",'ignore')) is not None
-	return re.search(ur"[\u3040-\u309f]+",s) is not None or re.search(ur'[\u30a0-\u30ff]+',s) is not None
+    #return re.search(ur"[\u3040-\u309f]+",unicode(s,"utf-8",'ignore')) is not None or re.search(ur"[\u30a0-\u30ff]+",unicode(s,"utf-8",'ignore')) is not None
+    return re.search(ur"[\u3040-\u309f]+", s) is not None or re.search(ur'[\u30a0-\u30ff]+', s) is not None
 
 
 '''
@@ -44,53 +47,63 @@ def isJapan(s):
 '''
 # 去除标点符号和无效字符
 def remove_meanless(instr):
-	patern=re.compile(r'[\r\n\t\s【】;:,：，；*?\]\[]?')
-	sr=re.sub(patern,'',instr)
-	return sr
+    patern = re.compile(r'[\r\n\t\s【】;:,：，；*?\]\[]?')
+    sr = re.sub(patern, '', instr)
+    return sr
+
 
 # 替换字符（用函数来判断规则）
 def resub():
-	s = 'i say, hello world!'
-	pattern = re.compile(r'(\w+) (\w+)')
-	print re.sub(pattern,r'\2 \1', s)
+    s = 'i say, hello world!'
+    pattern = re.compile(r'(\w+) (\w+)')
+    print re.sub(pattern, r'\2 \1', s)
 
-	# 嵌套定义
-	def func(m):
-		return m.group(1).title() + ' ' + m.group(2).title()
+    # 嵌套定义
+    def func(m):
+        return m.group(1).title() + ' ' + m.group(2).title()
 
-	print re.sub(pattern,func, s)
+    print re.sub(pattern, func, s)
+
 
 # 判断字符串是否是有意义串
 def judge_mean(instr):
-	sr=remove_meanless(instr)
-	if len(sr)==0:
-		return False
-	else:
-		return True
+    sr = remove_meanless(instr)
+    if len(sr) == 0:
+        return False
+    else:
+        return True
 
 
 # 字符串转字典
 def str_to_dict():
-	instr='name=zhang&age=12'
-	d=dict(map(lambda x:x.split('='),instr.split('&')))
-	dstr=str(d)
-	dstr_json=dstr.replace('\'','"')
+    instr = 'name=zhang&age=12'
+    d = dict(map(lambda x: x.split('='), instr.split('&')))
+    dstr = str(d)
+    dstr_json = dstr.replace('\'', '"')
 
-	print d
-	print dstr
-	print dstr_json	 #json解析的字符串必须是双引号括住的
+    print d
+    print dstr
+    print dstr_json  #json解析的字符串必须是双引号括住的
 
-	print json.loads(dstr_json)#,encoding='utf8')
-
-
+    print json.loads(dstr_json)  #,encoding='utf8')
 
 
+# 去除html标签
+def html_filter():
+    tag = '<h2 id="titiletext">Selenium技巧-截图相关-收集未整<span>这是span内容</span></h2>'
+    p = re.compile(r'<[^>]+>')
+    filtertag = p.sub("", tag)
+    print filtertag
+
+    from bs4 import BeautifulSoup
+    bstag=BeautifulSoup(tag)
+    print bstag.text
 
 
 # 测试入口
 if __name__ == "__main__":
-	#filter_str()
-	#judge_mean()
-	str_to_dict()
-
+    #filter_str()
+    #judge_mean()
+    #str_to_dict()
+    html_filter()
 
